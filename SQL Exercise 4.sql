08eb4fce57f7b6f1b2d073f6dca069ac13f3b942
Chi To <guko_1214@hotmail.com>
2024-01-28 22:51:59 +0800

    Update SQL Exercise 4

--- SQL Exercise 4.sql
+++ SQL Exercise 4.sql
@@ -33,32 +33,27 @@ insert into matches (match_id, first_player, second_player, first_score, second_
 (42,45,65,8,4);
 
 with scores as (
-select p.player_id, m.first_score as score
+select p.player_id, m.first_score as score, p.group_id
 from players p
 left join matches m on p.player_id = m.first_player
 union all
-select p.player_id, m.second_score as score
+select p.player_id, m.second_score as score, p.group_id
 from players p
 left join matches m on p.player_id = m.second_player
 ), scores2 as (
-	select s.player_id, sum(s.score) as score
+	select s.player_id, sum(s.score) as score,s.group_id
 	from scores s
-    group by s.player_id
-), score3 as (
-	select s2.player_id, s2.score, p.group_id
-    from scores2 s2
-    inner join players p on s2.player_id = p.player_id
+    group by s.player_id, s.group_id
 ), gp_max_score as (
-	select p.group_id, max(s2.score) as score
+	select s2.group_id, max(s2.score) as score
 	from scores2 s2
-	inner join players p on s2.player_id = p.player_id
-	group by p.group_id
+	group by s2.group_id
 )
 
-select gms.group_id, min(s3.player_id)
+select gms.group_id, min(s2.player_id)
 from gp_max_score gms
-left join score3 s3 on (gms.score = s3.score and gms.group_id = s3.group_id)
-or (gms.group_id = s3.group_id and s3.score is null and gms.score is null)
+left join scores2 s2 on (gms.score = s2.score and gms.group_id = s2.group_id)
+or (gms.group_id = s2.group_id and s2.score is null and gms.score is null)
 group by gms.group_id
 order by gms.group_id
 ;
